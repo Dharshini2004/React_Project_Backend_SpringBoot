ca72cf693b91b2a4dd48c10cefa1bbbe4a68ba22
Dharshini2004 <98155985+Dharshini2004@users.noreply.github.com>
2024-04-03 14:59:51 +0530

    Delete mainbackend directory

--- mainbackend/src/main/java/com/example/mainbackend/repository/MainRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.mainbackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.mainbackend.model.MainData;
-
-public interface MainRepo extends JpaRepository<MainData,Long>{
-    
-}
